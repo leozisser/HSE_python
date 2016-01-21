46bbc453bf5c2a60e37fdf03da5dece0c237c81a
leozisser <leozisser@gmail.com>
2016-01-21 14:34:52 +0300

    unfinished version

---
+++ amhar2.py
@@ -0,0 +1,20 @@
+__author__ = '123'
+import urllib.request, re, os.path
+b = urllib.request.Request('http://www.zavety-i.ru', headers = {'User-Agent':"Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"})
+a = urllib.request.urlopen(b)
+page = a.read().decode('utf-8')
+wwwpage = open("C:/coding/Python/internetpage.txt", 'w')
+hreflist = open("C:/coding/Python/hreflist.txt", 'w')
+wwwpage.write(page)
+#print(a.getcode)
+hrefs = []
+href = re.compile("\?module[^\']*")
+for line in page:   #почему не находит в page ничего?
+    batch = re.findall(href,line)#.split(href)
+    for a in batch:
+        if a not in hrefs:
+            hrefs.append(a)
+print (page)
+print("hrefs: ", hrefs)
+print("batch:", batch)
+
