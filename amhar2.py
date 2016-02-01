20f2c80706ffb1ab4f33056c030be71548cf3799
leozisser <leozisser@gmail.com>
2016-02-02 00:49:14 +0300

    just the same as amhar.py

--- amhar2.py
+++ amhar2.py
@@ -1,5 +1,6 @@
 __author__ = '123'
 import urllib.request, re, os.path
+hrefs = [] #all hrefs that are there on the site
 b = urllib.request.Request('http://www.zavety-i.ru', headers = {'User-Agent':"Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"})
 a = urllib.request.urlopen(b)
 page = a.read().decode('utf-8')
@@ -7,8 +8,9 @@ wwwpage = open("C:/coding/Python/internetpage.txt", 'w')
 hreflist = open("C:/coding/Python/hreflist.txt", 'w')
 wwwpage.write(page)
 #print(a.getcode)
-hrefs = []
-href = re.compile("\?module[^\']*")
+
+batch = []
+href = re.compile("\?module[^\"\']*")
 for line in page:   #почему не находит в page ничего?
     batch = re.findall(href,line)#.split(href)
     for a in batch:
@@ -17,4 +19,5 @@ for line in page:   #почему не находит в page ничего?
 print (page)
 print("hrefs: ", hrefs)
 print("batch:", batch)
-
+visited = []
+for link in hrefs:
