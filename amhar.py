e8efe3d8960105546509e9b2833a18f644c97fd4
leozisser <leozisser@gmail.com>
2016-02-02 00:48:20 +0300

    now it is finally working

--- amhar.py
+++ amhar.py
@@ -1,18 +1,27 @@
-__author__ = '123'
-import codecs
-amh = open ('C:/coding/Python/amhar.tsv','r', encoding='utf-8')
-chars = [line.split()for line in amh]
-amhdict = {}
-for y in chars:
-    for x in y:
-        amhdict[chars[int(x)][int(y)]]
 
-rerere = str(lines)
-res = open ('C:/coding/Python/amhtest.txt','w', encoding = 'utf-8')
-res.write(rerere)
-setpath = input("set path for in/out", )
-with open(setpath, 'r', encoding='utf-8') as inn:
-    for line in inn:
-        for
-for line in lines:
-    for i in line:
+amh = open ('C:/coding/Python/amhar.tsv','r', encoding='utf-8')
+a = [line.split()for line in amh]
+x = a[0]
+#print(x)
+y = []
+test = open ("C:/coding/Python/amhtest2.txt", "w", encoding="utf-8")
+for k in a:
+        y.append(k[0])
+
+b = {}
+for i in a[1:]:
+    for j in i[1:]:
+        b[j]=y[a.index(i)]+x[i.index(j)]
+#test.write(str(b))
+
+
+
+def amhar(line):
+
+   for char in b:
+      line = line.replace(char, b[char])
+   return line
+
+inn = open ('C:/coding/Python/amh.txt','r', encoding='utf-8')
+for line in inn:
+    test.write(amhar(line))
